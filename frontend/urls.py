bdec2cfc4e3036bd727d0c429c7515d0aa49afc7
Vidhya <vidhyadarade1@gmail.com>
2021-10-19 21:28:26 +0530

    login added

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name="homePage"),
+    path('login',views.customerLogin, name="customerLogin"),
     path('product/<int:product_id>', views.ProductDetails, name="ProductDetails"),
     path('category-product/<int:product_category_id>', views.CategoryProducts, name= "CategoryProducts" ),
 ]
